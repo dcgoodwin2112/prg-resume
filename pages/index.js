dcd3b903c4dc2f9af0ff6e59ee469a35d6bdf875
Daniel Goodwin <dcg2112@ad.unc.edu>
2021-03-26 19:48:17 -0400

    index page updates.

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
@@ -10,20 +11,27 @@ export default function Home({
   invitedTalks,
   funding,
 }) {
+  const scrollBreakpoint = 400;
+  const [scrollPos, setScrollPos] = useState("top");
+
+  useEffect(() => {
+    document.addEventListener("scroll", (e) => {
+      const scroll = window.scrollY;
+      if (scroll < scrollBreakpoint && scrollPos !== "top") {
+        setScrollPos("top");
+      } else if (scroll >= scrollBreakpoint && scrollPos !== "scroll") {
+        setScrollPos("scroll");
+      }
+    });
+  }, [scrollPos]);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Dr. Prairie Rose Goodwin</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className={styles.header}>
-        <Image src="/prairie-rose.jpeg" width="302px" height="403px" />
-        <div className={styles.contactInfo}>
-          <h1>Dr. Prairie Rose Goodwin</h1>
-          <p>Sr. Product Developer and Computer Science Professor</p>
-          <p>Apex, NC</p>
-        </div>
-      </header>
+      <Header scrollPos={scrollPos} />
       <main className={styles.main}>
         <div className={styles.grid}>
           <Section {...education} />
@@ -33,7 +41,27 @@ export default function Home({
           <Section {...funding} />
         </div>
       </main>
+      <footer className={styles.siteFooter}>
+        <hr />
+        &copy;{new Date().getFullYear()}
+      </footer>
     </div>
+  );
+}
+
+export function Header({ scrollPos }) {
+  return (
+    <header className={styles[`header-${scrollPos}`]}>
+      {scrollPos == "top" && (
+        <Image src="/prairie-rose.jpeg" width="262px" height="400px" />
+      )}
+
+      <div className={styles[`contact-info-${scrollPos}`]}>
+        <h1>Dr. Prairie Rose Goodwin</h1>
+        <p>Sr. Product Developer and Computer Science Professor</p>
+        {scrollPos == "top" && <p>Apex, NC</p>}
+      </div>
+    </header>
   );
 }
 
@@ -41,7 +69,10 @@ export function Section({ data: { title }, html }) {
   return (
     <section className={styles.section}>
       <h2 className={styles.sectionTitle}>{title}</h2>
-      <div className={styles.sectionContent} dangerouslySetInnerHTML={{ __html: html }}></div>
+      <div
+        className={styles.sectionContent}
+        dangerouslySetInnerHTML={{ __html: html }}
+      ></div>
     </section>
   );
 }
